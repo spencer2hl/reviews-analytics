9136836ee8f44f72bf4457a1b8b3108d10318843
spencer2hl <frost0901@outlook.com>
2019-05-01 15:59:27 -0500

    calc

--- read.py
+++ read.py
@@ -6,8 +6,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1 
 		if count % 1000 == 0: # % 求余数
 			print(len(data))
-print(len(data))
+print('档案读取完了，总共有', len(data), '笔资料')
 
-print(data[0])
-print('------------')
-print(data[1])
+sum_len = 0
+for d in data: 
+	sum_len += len(d)
+print('留言的平均长度为', sum_len / len(data))
