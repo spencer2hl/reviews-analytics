a41187a66b87ea4838dd0e1f72ecfdc452c6393e
spencer2hl <frost0901@outlook.com>
2019-05-01 16:08:24 -0500

    add filtering

--- read.py
+++ read.py
@@ -12,3 +12,11 @@ sum_len = 0
 for d in data: 
 	sum_len += len(d)
 print('留言的平均长度为', sum_len / len(data))
+# 作筛选
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '笔留言长度小于100')
+print(new[0])
+print(new[1])
